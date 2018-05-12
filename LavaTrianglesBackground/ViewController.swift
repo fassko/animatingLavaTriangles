eaa25ed4651d215edc7030802a1ee2d0aa274175
Kristaps Grinbergs <kristaps@qminderapp.com>
2018-05-12 21:07:17 +0200

    Initial Commit

---
+++ LavaTrianglesBackground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LavaTrianglesBackground
+//
+//  Created by Kristaps Grinbergs on 12/05/2018.
+//  Copyright © 2018 fassko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
