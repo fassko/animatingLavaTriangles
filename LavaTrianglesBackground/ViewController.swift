282b40d699c596e176f702316e863108843e37bd
Kristaps Grinbergs <fassko@gmail.com>
2019-10-12 11:12:35 +0300

    Update project

--- LavaTrianglesBackground/ViewController.swift
+++ LavaTrianglesBackground/ViewController.swift
@@ -15,10 +15,10 @@ class ViewController: UIViewController {
     
     let alphaLevels = [
       0.0,
-      0.05,
-      0.1,
+      0.15,
       0.2,
       0.3,
+      0.4,
       ].map { CGFloat($0) }
     
     let backgroundAnimationView = BackgroundView(
